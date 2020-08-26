491d594fb4f9248b59216889cb8a76298269334c
akaash@jonar.com <akaash@jonar.com>
2020-08-26 14:52:33 -0400

    fixed args to delete pendgins in Get

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -1,29 +1,66 @@
 pragma experimental ABIEncoderV2;
 
-contract Counter {    
-    mapping (address => mapping(address => string)) PatientToDoctor;
-    mapping (address => address[]) PatientToDoctorPending;
+contract Counter {
+    mapping(address => mapping(address => string)) PatientToDoctor;
+    mapping(address => address[]) PatientToDoctorPending;
 
-    function Set(address doctorAddress, string memory token) public returns(bool){
+    function Set(address doctorAddress, string memory token)
+        public
+        returns (bool)
+    {
         PatientToDoctor[msg.sender][doctorAddress] = token;
+        DeletePending(msg.sender, doctorAddress);
         return true;
     }
 
-    function Get(address patientAddress, address doctorAddress) external view returns(string memory){
-       return PatientToDoctor[patientAddress][doctorAddress];
+    function Get(address patientAddress, address doctorAddress)
+        external
+        view
+        returns (string memory)
+    {
+        return PatientToDoctor[patientAddress][doctorAddress];
     }
 
-    function SetPending(address doctorAddress) public returns(bool){
-        PatientToDoctorPending[msg.sender].push(doctorAddress);
+    function SetPending(address patientAddress, address doctorAddress) public returns (bool) {
+        for (
+            uint256 i = 0;
+            i < PatientToDoctorPending[patientAddress].length;
+            i++
+        ) {
+            if (PatientToDoctorPending[patientAddress][i] == doctorAddress) {
+                return false;
+            }
+        }
+        PatientToDoctorPending[patientAddress].push(doctorAddress);
         return true;
     }
 
-    function GetAllPending(address patientAddress) public view returns(address[] memory){
+    function GetAllPending(address patientAddress)
+        public
+        view
+        returns (address[] memory)
+    {
         return PatientToDoctorPending[patientAddress];
     }
 
-    function DeleteAllPending(address patientAddress) public returns(bool){
+    function DeleteAllPending(address patientAddress) public returns (bool) {
         delete PatientToDoctorPending[patientAddress];
+        return true;
+    }
+
+    function DeletePending(address patientAddress, address doctorAddress)
+        public
+        returns (bool)
+    {
+        for (
+            uint256 i = 0;
+            i < PatientToDoctorPending[patientAddress].length;
+            i++
+        ) {
+            if (PatientToDoctorPending[patientAddress][i] == doctorAddress) {
+                delete PatientToDoctorPending[patientAddress][i];
+            }
+        }
         return true;
     }
 }
